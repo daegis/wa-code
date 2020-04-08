5adc4d701abafcc121c24781b6bd029cbf647393
缐英达 <xianyingda@guazi.com>
2020-04-08 16:58:26 +0800

    Initial commit

---
+++ src/main/java/cn/aegisa/wow/wa/WaCodeApplication.java
@@ -0,0 +1,13 @@
+package cn.aegisa.wow.wa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/aegisa/wow/wa/WaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.aegisa.wow.wa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
